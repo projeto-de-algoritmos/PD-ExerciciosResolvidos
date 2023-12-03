1a071316e6b42ce278062d0ed1d36c7cd65e1e35
Luiz Henrique Fernandes Zamprogno <62183792+luiz-hfz@users.noreply.github.com>
2023-12-03 15:46:24 -0300

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        s = ' ' + s
+        p = ' ' + p
+        output = [[False]*len(p) for i in range(len(s))]
+        output[0][0] = True
+
+        for row in range(len(s)):
+            for col in range(1, len(p)):
+                if p[col] != '*':
+                    if p[col] == s[row] or p[col] == '.' and row != 0:
+                        output[row][col] = output[row-1][col-1]
+                else:
+                    output[row][col] = output[row][col-2] 
+                    if (not output[row][col]) and ((s[row] == p[col - 1]) or p[col-1] == '.'):
+                        output[row][col] = output[row-1][col]
+
+        return output[-1][-1]
